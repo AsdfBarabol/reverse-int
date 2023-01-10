c7505175bf0879fbaa11a5f3b898499a11ffa63f
Andrey Barabolya <andrey_ba@tut.by>
2023-01-10 23:32:14 +0300

    fix: negativ number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-    return Number(String(n).split("").reverse().join(""));
+    return (n>=0)?(String(n).split("").reverse().join("")):(String(n*(-1)).split("").reverse().join(""));
 }
